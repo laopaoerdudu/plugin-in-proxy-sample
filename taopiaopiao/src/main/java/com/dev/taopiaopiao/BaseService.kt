d6bdcca5c76d078b7349d99f74c0329569047a91
David <18588744489@163.com>
2021-09-05 16:51:29 +0800

    Update

--- taopiaopiao/src/main/java/com/dev/taopiaopiao/BaseService.kt
+++ taopiaopiao/src/main/java/com/dev/taopiaopiao/BaseService.kt
@@ -1,12 +1,10 @@
 package com.dev.taopiaopiao
 
-import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import com.dev.pluginstand.IService
 
-@SuppressLint("MissingSuperCall")
 open class BaseService : Service(), IService {
     private lateinit var who: Service
 

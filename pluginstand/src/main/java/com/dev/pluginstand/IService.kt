bcba348670ae9fbe94adbd830b0a08ff9bba129e
David <18588744489@163.com>
2021-09-19 12:22:14 +0800

    Remove useless function

--- pluginstand/src/main/java/com/dev/pluginstand/IService.kt
+++ pluginstand/src/main/java/com/dev/pluginstand/IService.kt
@@ -2,19 +2,12 @@ package com.dev.pluginstand
 
 import android.app.Service
 import android.content.Intent
-import android.content.res.Configuration
 import android.os.IBinder
 
 interface IService {
     fun attach(service: Service)
     fun onCreate()
     fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int
-    fun onConfigurationChanged(newConfig: Configuration)
     fun onBind(intent: Intent?): IBinder?
-    fun onRebind(intent: Intent?)
     fun onUnbind(intent: Intent?): Boolean
-    fun onTaskRemoved(rootIntent: Intent?)
-    fun onLowMemory()
-    fun onTrimMemory(level: Int)
-    fun onDestroy()
 }
